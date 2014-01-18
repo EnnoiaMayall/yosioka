c437b94c506c97560f7a1cc4c100b8b93e72a09d
enoa <ennoia8@gmail.com>
2014-01-18 18:17:05 +0900

    文字の課題

--- 2013_12_21.c
+++ 2013_12_21.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 
-
-a;
-
 int main()
 {
 	char s[2];
 	
-	int i = 0;
-	
-	printf("%d",a);
-	
 	while(fgets(s,sizeof(s),stdin) != NULL)
 	{
-		//fputs(s,stdout);
-		printf("[%s]",s);
-		
-		i++;
-		if(i == sizeof(stdout) )
-		{
-			i = 0;
+		if( s[0] == ' ' ) {
 			printf("\n");
+			continue;
 		}
+		printf("[%s]",s);
 	}
 	return 0;
 }
